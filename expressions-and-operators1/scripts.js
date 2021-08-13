d375d013f37bd573c38b0211d0a6f0803e09447a
MateusJSouza <mateusjdev@gmail.com>
2021-08-13 17:10:00 -0300

    expressions and operators

---
+++ expressions-and-operators1/scripts.js
@@ -0,0 +1,26 @@
+/* Expressões e operadores
+
+- Expressions 
+- Operatars
+  Binary
+  Unary
+  Ternary
+*/
+
+let number = 1;
+
+(function() {
+  console.log('alo'); // resultado -> 'alo'
+})()
+
+// Ternário
+console.log(false ? 'alo' : 'nada');
+
+let number2 = 1
+console.log(number2 + 1); // resultado -> 2
+
+// Incremento
+console.log(++number); // resultado -> 2
+
+// Decremento
+console.log(--number); // resultado -> 1
