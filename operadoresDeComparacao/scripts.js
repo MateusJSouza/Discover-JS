707322c3d3863d48d0c37589ea609c47165bbffa
MateusJSouza <mateusjdev@gmail.com>
2022-01-17 13:13:57 -0300

    Grupo de operadores e operadores de comparação

---
+++ operadoresDeComparacao/scripts.js
@@ -0,0 +1,23 @@
+// Operadores de Comparação
+
+// Irá comprovar valores e retornar um boolean como resposta à comparação
+
+let one = 1
+let two = 2
+
+// == Igual a
+console.log( two == 1)
+console.log( two == '1')
+
+// != diferente de 
+console.log(one != two)
+console.log(one != 1)
+console.log(two != 2)
+
+// === estritamente igual a 
+console.log( one === '1')
+console.log( one === 1)
+
+// !== estritamente diferente de
+console.log( two !== '2')
+console.log( two !== 2)
